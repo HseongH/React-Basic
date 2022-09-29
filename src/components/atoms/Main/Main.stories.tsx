259636ac47b91a26f00ce0b196d0b0a126e05308
HseongH <hh4518@gmail.com>
2022-09-29 19:28:50 +0900

    add main component story type

--- src/components/atoms/Main/Main.stories.tsx
+++ src/components/atoms/Main/Main.stories.tsx
@@ -1,11 +1,12 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Main from ".";
 
 export default {
   title: "Atoms/Main",
   component: Main,
-};
+} as ComponentMeta<typeof Main>;
 
-const Template = () => (
+const Template: ComponentStory<typeof Main> = () => (
   <Main>
     <h1>Welcome To React</h1>
   </Main>
--- src/components/atoms/Main/Main.style.ts
+++ src/components/atoms/Main/Main.style.ts
@@ -1,20 +1,16 @@
 import styled from "@emotion/styled";
 
-const StyledMain = styled.main(({ theme }) => {
-  console.log(theme);
-
-  return {
-    maxWidth: theme.pageWidth.xl,
-    minHeight: "100vh",
-    margin: "0 auto",
-    position: "relative",
-    backgroundColor: theme.palette.common.main,
-    color: theme.palette.common.text,
-    fontSize: theme.typography.fontSize,
-    fontWeight: theme.typography.fontWeight,
-    fontFamily: theme.typography.fontFamily,
-    lineHeight: theme.typography.lineHeight,
-  };
-});
+const StyledMain = styled.main(({ theme }) => ({
+  maxWidth: theme.pageWidth.xl,
+  minHeight: "100vh",
+  margin: "0 auto",
+  position: "relative",
+  backgroundColor: theme.palette.common.main,
+  color: theme.palette.common.text,
+  fontSize: theme.typography.fontSize,
+  fontWeight: theme.typography.fontWeight,
+  fontFamily: theme.typography.fontFamily,
+  lineHeight: theme.typography.lineHeight,
+}));
 
 export default StyledMain;
